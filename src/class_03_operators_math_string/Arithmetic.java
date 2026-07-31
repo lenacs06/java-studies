597b72f41123e98aea8dba08d2a5f826fee87a44
lenacs06 <helenacs2006@gmail.com>
2026-07-31 02:41:07 -0300

    feat: adding content to arithmetic file

---
+++ src/class_03_operators_math_string/Arithmetic.java
@@ -0,0 +1,33 @@
+package class_03_operators_math_string;
+public class Arithmetic {
+    static void main (String[] args){
+        // Arithmetic Operators. Example.
+
+        int x = 10;
+        int y = 3;
+
+        System.out.println(x + y); // 13
+        System.out.println(x - y); // 7
+        System.out.println(x * y); // 30
+        System.out.println(x / y); // 3
+        System.out.println(x % y); // 1
+
+        int z = 5;
+        ++z;
+        System.out.println(z); // 6
+        --z;
+        System.out.println(z); // 5
+
+        // Incrementing and Decrementing. Example.
+
+        //Incrementing
+        int b = 67;
+        ++b;
+        System.out.println(b); // 68
+
+        //Decrementing
+        int v = 80;
+        --v;
+        System.out.println(v); // 79
+    }
+}
